22529a8ee8dd9398786c987bafa2fac719b2d069
ndudefaces <121698953+ndudefaces@users.noreply.github.com>
2023-03-31 22:37:11 -0500

    Add createGrid function

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const container = document.querySelector('#container');
+const sizeofGrid = 16;
+const container = document.querySelector('.container');
 
-function createBoxes(numBox) {
-  for (let i = 0; i < numBox; i++) {
-    const row = container.appendChild(document.createElement('div'));
-    for (let j = 0; j < numBox; j++) {
-      const square = document.createElement('div');
-      square.className = 'box';
-      row.appendChild(square);
+const createGrid = (amtOfGrids) => {
+  for (let i = 0; i < amtOfGrids; i++) {
+    const row = document.createElement('div');
+    row.classList.add('grid-row');
+
+    for (let j = 0; j < amtOfGrids; j++) {
+      const gridBox = document.createElement('div');
+      gridBox.classList.add('grid-box');
+      row.appendChild(gridBox);
     }
+
+    container.appendChild(row);
   }
 }
 
-
-createBoxes(16);
+createGrid(sizeofGrid);
