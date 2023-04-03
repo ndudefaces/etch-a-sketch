5dd7bda3f785212108fa17389af152b2f9ea45a7
ndudefaces <121698953+ndudefaces@users.noreply.github.com>
2023-04-02 19:09:38 -0500

    Add logic and styling for dynamic box width/height

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const createGrid = (amtOfGrids) => {
     row.classList.add('grid-row');
 
     for (let j = 0; j < amtOfGrids; j++) {
+      const widthAndHeight = 960 / sizeofGrid;
       const gridBox = document.createElement('div');
       gridBox.classList.add('grid-box');
+      gridBox.style.width = `${widthAndHeight}px`;
+      gridBox.style.height = `${widthAndHeight}px`;
       row.appendChild(gridBox);
     }
 
